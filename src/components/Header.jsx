eb7f7810532ea02d98e03bd3d289a4e4d71ab04c
Vladimir <vovik261096@mail.ru>
2021-12-15 13:07:08 +0700

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+    <img src='https://uprostim.com/wp-content/uploads/2021/05/image032-6.jpg'/>
+    <h1>Привет,люди</h1>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Pofile.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+    <div>
+      <img className='content_img' src='https://phonoteka.org/uploads/posts/2021-04/1619064028_17-phonoteka_org-p-dlinnie-kartinki-dlya-fona-18.jpg'/>
+    </div>
+    <div>
+      <img className='content_avatar' src='https://mykaleidoscope.ru/uploads/posts/2020-03/1584622396_28-p-krasivie-zagorodnie-doma-92.jpg'/>
+      
+    </div>
+  
+    <div>
+      My posts
+        <div>New post</div>
+        <div>Post1</div>
+        <div>Post2</div>
+        <div>Post3</div>
+    </div>
+  </div>
+}
+
+export default Profile;
